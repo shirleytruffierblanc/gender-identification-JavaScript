ba1be4e838a2cee7248d70b8d0ea51454641a73d
shirleytruffierblanc <40561239+shirleytruffierblanc@users.noreply.github.com>
2018-10-09 09:13:01 -0500

    Update gender-identification-JavaScript.js

--- gender-identification-JavaScript.js
+++ gender-identification-JavaScript.js
@@ -1,15 +1,22 @@
 //code in JavaScript
+#!/usr/bin/env node
 
-var fs = require('fs');
-var str = fs.readFileSync('input.txt', 'utf8');
+//permit to use all type of file name
+let chunk = "";
 
-str.replace(/^\s*[\r\n]/gm,"").split(/\s+/).forEach(function (s) {
-    return console.log(
-	s === 'bob'
-	    ? 'boy'
-	    : s === 'alicia'
-	    ? 'girl'
-	    : s === 'cookie'
-	    ? 'dog'
-	    : 'unknown');
+process.stdin.on("data", data => {
+    chunk += data.toString();
+});
+
+process.stdin.on("end", () => {
+    chunk.replace(/^\s*[\r\n]/gm,"").split(/\s+/).forEach(function (s) {
+        process.stdout.write(
+        s === 'bob'
+        ? 'boy \n'
+        : s === 'alicia'
+           ? 'girl\n'
+           : s === 'cookie'
+               ? 'dog \n'
+               : 'unknown \n');
+    });
 });
